8d3d937e2a340980beb2b3b0d135936dc4aa6c59
Vladimir Azanza <vladimirazanza@gmail.com>
2024-06-01 19:19:45 +0500

    Add Class Based Views for every feature

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,49 +1,26 @@
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import CreateView, ListView
+# from django.core.paginator import Paginator
+# from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-from .utils import calculate_birthday_countdown
+# from .utils import calculate_birthday_countdown
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayMixin:
     model = Birthday
     form_class = BirthdayForm
     template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(
-            Birthday,
-            pk=pk
-        )
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update(
-            {
-                'birthday_countdown': birthday_countdown
-            }
-        )
-    return render(
-        request,
-        'birthday/birthday.html',
-        context
-    )
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    pass
 
 
 class BirthdayListView(ListView):
@@ -51,6 +28,45 @@ class BirthdayListView(ListView):
     ordering = 'id'
     paginate_by = 10
 
+
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+'''
+    Below the same code as BirthdayCreate(Update)View but written as
+    a view function:
+    def birthday(request, pk=None):
+        if pk is not None:
+            instance = get_object_or_404(
+                Birthday,
+                pk=pk
+            )
+        else:
+            instance = None
+        form = BirthdayForm(
+            request.POST or None,
+            files=request.FILES or None,
+            instance=instance
+        )
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            birthday_countdown = calculate_birthday_countdown(
+                form.cleaned_data['birthday']
+            )
+            context.update(
+                {
+                    'birthday_countdown': birthday_countdown
+                }
+            )
+        return render(
+            request,
+            'birthday/birthday.html',
+            context
+        )
+'''
 
 '''
     Below the same code as BirthdayListView but written as a view function:
@@ -69,23 +85,25 @@ class BirthdayListView(ListView):
         )
 '''
 
-
-def delete_birthday(request, pk):
-    instance = get_object_or_404(
-        Birthday,
-        pk=pk
-    )
-    form = BirthdayForm(
-        instance=instance
-    )
-    context = {
-        'form': form
-    }
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(
-        request,
-        'birthday/birthday.html',
-        context
-    )
+'''
+    Below the same code as BirthdayListView but written as a view function:
+    def delete_birthday(request, pk):
+        instance = get_object_or_404(
+            Birthday,
+            pk=pk
+        )
+        form = BirthdayForm(
+            instance=instance
+        )
+        context = {
+            'form': form
+        }
+        if request.method == 'POST':
+            instance.delete()
+            return redirect('birthday:list')
+        return render(
+            request,
+            'birthday/birthday.html',
+            context
+        )
+'''
